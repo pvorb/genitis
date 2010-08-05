8f47ebf6160ce4e4530552eaabf4922013bef007
pvorb <vorbach@genitis.org>
2010-08-05 15:21:47 +0200

    Updated rules for redirections and a rule in .htaccess.

--- lib/redirections.php
+++ lib/redirections.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Descriptive Text goes here...
- *
- * @author Paul Vorbach <vorbach@genitis.org>
- * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 0.1.0
- * @package org.genitis.content
- */
-
 $redirections = array(
-	'dev/' => 'projects/',
-	'dev/web/g08/' => 'projects/web/g08/',
-	'dev/sia/' => 'projects/edu/sia/',
+	'dev/' => 'project/',
+	'dev/web/g08/' => 'project/web/g08/',
+	'dev/sia/' => 'project/edu/sia/',
+	'res/doc/sia/dokumentation.pdf' => 'project/edu/sia/doc/documentation.pdf',
+	'res/doc/sia/praesentation.pdf' => 'project/edu/sia/doc/presentation.pdf',
 	'blog/' => 'log/'
 );
