85b560899f913ddc3647bcd08511dcad93c203f4
pvorb <vorbach@genitis.org>
2010-08-05 17:01:59 +0200

    Changed redirections.

--- lib/redirections.php
+++ lib/redirections.php
@@ -3,6 +3,7 @@ $redirections = array(
 	'dev/' => 'project/',
 	'dev/web/g08/' => 'project/web/g08/',
 	'dev/sia/' => 'project/edu/sia/',
+	'src/css/default.css' => 'res/css/alma.css',
 	'res/doc/sia/dokumentation.pdf' => 'project/edu/sia/doc/documentation.pdf',
 	'res/doc/sia/praesentation.pdf' => 'project/edu/sia/doc/presentation.pdf',
 	'blog/' => 'log/'
