06eaa6cdfd675a9d3ac17b8890ac10e82b1ecb59
Paul Vorbach <vorbach@genitis.org>
2010-11-02 02:28:49 +0100

    Fixed error in .htaccess file.
    Error caused 500 Internal Server Error when file was not found.

--- files/conf.php
+++ files/conf.php
@@ -1,24 +1,48 @@
 <?php
 /**
- * Configuration file
+ * Configuration file.
+ *
+ * The following definitions are needed:
+ * DOMAIN		URL to the domain of the site. Errors will redirect to
+ * 				‘DOMAIN/ERROR_404?s=…’.
+ * DEFAULT_FILE	default file in a directory that is included.
+ * ERROR_404	path to the 404 error page
+ * ENDL			character(s) that will be used when inserting a line break.
+ * INDENT		character(s) that will be used for indentation.
+ * DATE_FORMAT	default date format used by function date.
+ * TIME_FORMAT	default time format used by function date.
+ *
+ * The following variables are needed:
+ * $file_ext                    array of strings with all file extensions that
+ *                              will be looked for.
+ * $modules                     array of strings with all file names that shall
+ *                              be included relative to the 'lib/mod' folder.
  *
  * @author Paul Vorbach <vorbach@genitis.org>
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 0.1.0
  * @package org.genitis.yuki
  */
+define('DIR_LIB', dirname(__FILE__).DIR_SEP); // Don’t change!
 
+// -----------------
+// User definitions:
+// -----------------
 define('DOMAIN', 'http://www.genitis.org');
 
-define('DIR_LIB', dirname(__FILE__).DIR_SEP);
-
+define('DEFAULT_FILE', 'index');
 define('ERROR_404', 'error/404');
 define('ENDL', "\n");
 define('INDENT', "\t");
 
+define('DATE_FORMAT', 'M d, Y');
+define('TIME_FORMAT', 'g:m a');
+
 $file_ext = array(
 	'.html',
 	'.php'
+);
+
+$modules = array(
 );
 
 require_once 'functions.php';
