1838f8122cec3e59626fe0306ab3dc6599fe805c
pvorb <vorbach@genitis.org>
2011-01-14 14:26:31 +0100

    Removed google-search.js

--- html/genitis/res/js/google-search.js
+++
@@ -1,8 +0,0 @@
-/**
- * Script that provides an interface to include search result from Google. 
- *
- * @author Paul Vorbach <vorbach@genitis.org>
- * @license http://opensource.org/licenses/mit-license.php MIT License
- */
-
-
