6356bca8d1c3ab806bea2f5cdadc7765678e5bf3
pvorb <vorbach@genitis.org>
2011-03-01 17:28:16 +0100

    Completion of HTML5 test and reconstruction.

--- html/genitis/manager.php
+++ html/genitis/manager.php
@@ -7,38 +7,75 @@
  * @package org.genitis.yuki
  */
 
-// Requires conf.php
 require 'conf.php';
 require DIR_LIB.'functions.php';
 
-// Exit, if GET parameter 'url' is not set
-if (!isset($_GET['url']))
-	exit;
-
-$url = $_GET['url']; // request
-
-// Check for GET parameter 'file'
-if (isset($_GET['file'])) {
-	// Load necessary modules
-	load_modules($modules);
-	// Include file
-	get_file($url);
+// Check for prefix redirections.
+require DIR_PUB.'redirect.php';
+$url = $_GET['url'];
+foreach ($redir_prefix as $from => $to) {
+	if (strpos($url.'/', $from) === 0) {
+		$i = 1;
+		$goto = str_replace($from, $to, $url.'/', $i);
+		redirect(301, $goto, FALSE, (strpos($goto, '//') != FALSE));
+		exit;
+	}
 }
-// Check for GET parameter 'dir'
-elseif (isset($_GET['dir'])) {
-	// Load necessary modules
-	load_modules($modules);
-	// Include dir
-	get_dir($url);
-}
-// Check for GET parameter 'err'
-elseif (isset($_GET['err'])) {
-	require 'redirect.php';
 
-	// If a redirection for $url is defined, make a redirect as defined.
-	if (isset($redirections[$url]))
-		redirect(301, $redirections[$url]);
-	// Otherwise redirect to a 404 error.
-	else
-		redirect(404, ERROR_404, $url);
+// Initialize $path
+$url = '';
+
+// Check for GET variable 'sub', which means a sumdomain.
+if (isset($_GET['sub'])) {
+	// Append the subdomain directory to $path.
+	$url .= $subdomains[$_GET['sub']];
+}
+
+// Append the url part to $path.
+$url .= $_GET['url'];
+
+// If $path is a directory
+if (is_dir(DIR_PUB.$url)) {
+	// include its default file.
+	include_file($url.DEFAULT_FILE);
+}
+// If it is an existing file
+elseif (file_exists(DIR_PUB.$url)) {
+	// get the file extension.
+	$info = explode('.', $url);
+	$ext = $info[count($info) - 1];
+
+	// Get the content type from the file.
+	if (isset($content_types[$ext])) {
+		// Send the 'Content-Type' header.
+		$ct = $content_types[$ext];
+		header('Content-Type: '.$ct);
+
+		$tmp = explode('/', $ct);
+		// If the content type is not a text
+		if ($tmp[0] != 'text') {
+			// send 'Last-Modified' header.
+			header('Last-Modified: '.date('r', filemtime(DIR_PUB.$url)));
+			// Directly read the file contents to output buffer.
+			readfile(DIR_PUB.$url);
+		} else {
+			// Interprete the file.
+			include DIR_PUB.$url;
+		}
+	} else {
+		// if content type is not defined throw HTTP 403 error (forbidden)
+		redirect(403, ERROR_403, $url);
+	}
+} else {
+	// Load modules and include the file.
+	load_modules($modules);
+	include_file($url);
+
+	// Redirect, if there is an redirection exactly for this file.
+	if (isset($redirects[$url])) {
+		redirect(301, $redir[$url], FALSE, (strpos($goto, '//') != FALSE));
+	}
+
+	// Redirect to the 404 error page.
+	redirect(404, ERROR_404, $url);
 }
