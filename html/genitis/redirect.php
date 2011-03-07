bafdcf980b0a385d85761459e9974022ebd07cfe
pvorb <vorbach@genitis.org>
2011-03-07 17:51:53 +0100

    Redirection about/version/ -> about/versions/.

--- html/genitis/redirect.php
+++ html/genitis/redirect.php
@@ -1,18 +1,20 @@
 <?php
 $redir_prefix = array(
-	'project/'	=> 'projects/',
-	'projects/web/'	=> 'projects/',
-	'projects/edu/'	=> 'projects/',
-	'dev/'		=> 'projects/',
-	'blog/'		=> 'log/'
+	'project/'		=> 'projects/',
+	'projects/web/'		=> 'projects/',
+	'projects/edu/'		=> 'projects/',
+	'dev/'			=> 'projects/',
+	'blog/'			=> 'log/',
+	'about/version/'	=> 'about/versions/'
 );
 
 $redir = array(
-	'projects/edu/sia/' => 'projects/sia/',
-	'projects/g08' => 'projects/ganerben2008.de',
-	'projects/pv' => 'projects/paulvorbach.de',
-	'about/contact' => 'about/legal',
-	'src/css/default.css' => 'res/css/alma.css',
-	'res/doc/sia/dokumentation.pdf' => 'projects/sia/doc/documentation.pdf',
-	'res/doc/sia/praesentation.pdf' => 'projects/sia/doc/presentation.pdf'
+	'projects/edu/sia/'	=> 'projects/sia/',
+	'projects/g08'		=> 'projects/ganerben2008.de',
+	'projects/pv'		=> 'projects/paulvorbach.de',
+	'about/contact'		=> 'about/legal',
+	'about/versions/2'	=> 'about/versions/2.1',
+	'src/css/default.css'	=> 'res/css/alma.css',
+	'res/doc/sia/dokumentation.pdf'	=> 'projects/sia/doc/documentation.pdf',
+	'res/doc/sia/praesentation.pdf'	=> 'projects/sia/doc/presentation.pdf'
 );
